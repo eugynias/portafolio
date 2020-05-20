2577537569eacfe742d180ef39a2baba1286a097
unknown <eugynias>
2020-05-20 15:19:32 -0400

    Updates

---
+++ precache-manifest.e1c25333461a0d1450f0ca5dc27d0659.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376c03eec60973b175de1550efe36fe1",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "404e4985f27fcbf24151",
+    "url": "/portafolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "404e4985f27fcbf24151",
+    "url": "/portafolio/static/js/2.257f5301.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portafolio/static/js/2.257f5301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537f51b2c64628330b98",
+    "url": "/portafolio/static/js/main.e6b6bfb7.chunk.js"
+  },
+  {
+    "revision": "ed7a03c9a17b9159cc47",
+    "url": "/portafolio/static/js/runtime-main.2bd87577.js"
+  },
+  {
+    "revision": "425ec4279d784e81b900a9b540ffb7d4",
+    "url": "/portafolio/static/media/eugenianajar.425ec427.jpg"
+  },
+  {
+    "revision": "2df563feb290102416258421d4cafa9e",
+    "url": "/portafolio/static/media/fondo.2df563fe.jpg"
+  },
+  {
+    "revision": "b6ef68f89b3ccc2e8f7b2c160ee45f11",
+    "url": "/portafolio/static/media/lab-notes.b6ef68f8.png"
+  },
+  {
+    "revision": "32b16fcceba572906e3aadf5a5d97f30",
+    "url": "/portafolio/static/media/mdlinks4.32b16fcc.png"
+  },
+  {
+    "revision": "2f3be9b184275c9d111aee2899cc760f",
+    "url": "/portafolio/static/media/movie.2f3be9b1.png"
+  }
+]);
